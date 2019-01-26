25311247bc1d8c7eb2b10177eb31d7326b9b4a8e
mohit-mittal <mittalmohitmittal93@gmail.com>
2019-01-26 14:51:13 +0530

    Add main menu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+    public Button playButton;
+    public InputField userName;
+    public string MainScene;
+
+    private void Start() {
+        playButton.OnClickAsObservable()
+            .Subscribe(_ => {
+                if (!string.IsNullOrWhiteSpace(userName.text)) {
+                    PlayerPrefs.SetString("userName", userName.text);
+                    playButton.interactable = false;
+                    SceneManager.LoadSceneAsync(MainScene);
+                }
+            });
+    }
+}
