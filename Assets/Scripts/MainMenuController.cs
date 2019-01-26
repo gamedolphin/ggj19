0dc233b1a1622d6e50ced98b2fbef6a45b58d8e1
mohit-mittal <mittalmohitmittal93@gmail.com>
2019-01-26 17:06:00 +0530

    add name lenngth check

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -4,16 +4,37 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
+
+    public const string userName = "userName";
+
     public Button playButton;
-    public InputField userName;
+    public InputField userNameField;
     public string MainScene;
 
+    private void Awake() {
+        userNameField.text = PlayerPrefs.GetString(userName);
+        if (string.IsNullOrWhiteSpace(userNameField.text)) {
+            playButton.interactable = false;
+        } else {
+            playButton.interactable = true;
+        }
+    }
+
     private void Start() {
+        userNameField.OnValueChangedAsObservable()
+            .Subscribe(name => {
+                if (string.IsNullOrWhiteSpace(name) || name.Length > 10) {
+                    playButton.interactable = false;
+                } else {
+                    playButton.interactable = true;
+                }
+            });
+
         playButton.OnClickAsObservable()
             .Subscribe(_ => {
-                if (!string.IsNullOrWhiteSpace(userName.text)) {
-                    PlayerPrefs.SetString("userName", userName.text);
+                if (!string.IsNullOrWhiteSpace(userNameField.text)) {
                     playButton.interactable = false;
+                    PlayerPrefs.SetString(userName, userNameField.text);
                     SceneManager.LoadSceneAsync(MainScene);
                 }
             });
