fabac3c367cc668aca89e39317a8ca591a84d784
mohit-mittal <mittalmohitmittal93@gmail.com>
2019-01-26 15:55:01 +0530

    add indicator system

---
+++ Assets/Scripts/OffscreenIndicator/OffScreenIndicators.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace OffScreenIndicator
+{
+    public class OffScreenIndicators : MonoBehaviour
+    {
+        private Camera mainCamera;
+        private Vector3 screenCentre;
+        private Vector3 screenBounds;
+        private string targetTag = "Target";
+
+        [Range(0.5f, 0.9f)]
+        public float screenBoundOffset = 0.9f;
+
+        void Awake()
+        {
+            mainCamera = Camera.main;
+            screenCentre = new Vector3(Screen.width, Screen.height, 0) / 2;
+            screenBounds = screenCentre * screenBoundOffset;
+        }
+
+        void Update()
+        {
+            DeactivateAllIndicators();
+        }
+
+        void LateUpdate()
+        {
+            Paint();
+        }
+
+        void Paint()
+        {
+            GameObject[] objects = GameObject.FindGameObjectsWithTag(targetTag);
+            List<Target> targets = new List<Target>();
+            objects.ToList().ForEach(obj =>
+            {
+                Target target = obj.GetComponent<Target>();
+                if (target) { targets.Add(target); }
+            });
+
+            foreach (Target target in targets)
+            {
+                Vector3 screenPosition = OffScreenIndicatorCore.GetScreenPosition(mainCamera, target.transform.position);
+                bool isTargetVisible = OffScreenIndicatorCore.IsTargetVisible(screenPosition);
+                float distanceFromCamera = target.NeedDistanceText ? target.GetDistanceFromCamera(mainCamera.transform.position) : float.MinValue;
+                Indicator indicator = null;
+
+                if (target.NeedBoxIndicator && isTargetVisible)
+                {
+                    screenPosition.z = 0;
+                    indicator = BoxObjectPool.current.GetPooledObject();
+                }
+                else if (target.NeedArrowIndicator && !isTargetVisible)
+                {
+                    float angle = float.MinValue;
+                    OffScreenIndicatorCore.GetArrowIndicatorPositionAndAngle(ref screenPosition, ref angle, screenCentre, screenBounds);
+                    indicator = ArrowObjectPool.current.GetPooledObject();
+                    indicator.transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+                }
+                if (indicator)
+                {
+                    indicator.SetImageColor(target.TargetColor);
+                    indicator.SetDistanceText(distanceFromCamera);
+                    indicator.transform.position = screenPosition;
+                    indicator.SetTextRotation(Quaternion.identity);
+                    indicator.Activate(true); 
+                }
+            }
+        }
+
+        private void DeactivateAllIndicators()
+        {
+            BoxObjectPool.current.DeactivateAllPooledObjects();
+            ArrowObjectPool.current.DeactivateAllPooledObjects();
+        }
+    }
+}
---
+++ Assets/Scripts/OffscreenIndicator/Target.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace OffScreenIndicator
+{
+    public class Target : MonoBehaviour
+    {
+        [SerializeField] private Color targetColor;
+        [SerializeField] private bool needBoxIndicator = true;
+        [SerializeField] private bool needArrowIndicator = true;
+        [SerializeField] private bool needDistanceText = true;
+        private string targetTag = "Target";
+
+        public Color TargetColor
+        {
+            get
+            {
+                return targetColor;
+            }
+        }
+
+        public bool NeedBoxIndicator
+        {
+            get
+            {
+                return needBoxIndicator;
+            }
+        }
+
+        public bool NeedArrowIndicator
+        {
+            get
+            {
+                return needArrowIndicator;
+            }
+        }
+
+        public bool NeedDistanceText
+        {
+            get
+            {
+                return needDistanceText;
+            }
+        }
+
+        private void Awake()
+        {
+            gameObject.tag = targetTag;
+        }
+
+        public float GetDistanceFromCamera(Vector3 cameraPosition)
+        {
+            float distanceFromCamera = Vector3.Distance(cameraPosition, transform.position);
+            return distanceFromCamera;
+        }
+    }
+}
